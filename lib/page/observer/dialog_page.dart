d5629046649f4fac52729aa8f096e1bab2338b0a
林创湖 <linchuanghu@addcn.com>
2023-08-03 11:52:04 +0800

    示例：路由监听

--- lib/page/observer/dialog_page.dart
+++ lib/page/observer/dialog_page.dart
@@ -6,23 +6,28 @@ class DialogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          Go.dialog(
-            AlertDialog(
-              title: const Text('Dialog'),
-              content: const Text('This is a dialog.'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Go.back(),
-                  child: const Text('OK'),
-                ),
-              ],
-            ),
-          );
-        },
-        child: const Text('Show Dialog'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('路由监听'),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Go.dialog(
+              AlertDialog(
+                title: const Text('Dialog'),
+                content: const Text('This is a dialog.'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Go.back(),
+                    child: const Text('OK'),
+                  ),
+                ],
+              ),
+            );
+          },
+          child: const Text('Show Dialog'),
+        ),
       ),
     );
   }
