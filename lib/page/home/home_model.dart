9fd23674cf517af2a64a7bf37b8e08fba5ba9ed9
林创湖 <linchuanghu@addcn.com>
2023-08-03 10:07:16 +0800

    示例新增： Navigator2.0 web端示例（IndexedStack和ShellRoute）

--- lib/page/home/home_model.dart
+++ lib/page/home/home_model.dart
@@ -11,7 +11,11 @@ class HomeModel extends BaseModel {
     "路由 Navigator 2.0（StatefulShellRoute）: TabBar和TabBarView同步切换，支持滑动页面切换浏览器地址":
         GetPlatform.isWeb ? Routes.tab1 : Routes.tab,
     "路由 Navigator 2.0（StatefulShellRoute）: BottomNavigationBar和PageView同步切换，支持滑动页面切换浏览器地址":
-        GetPlatform.isWeb ? Routes.nav1 : Routes.nav,
+        GetPlatform.isWeb ? Routes.page1 : Routes.page,
+    "路由 Navigator 2.0（StatefulShellRoute.indexedStack）: BottomNavigationBar驱动IndexedStack内部页面切换":
+        GetPlatform.isWeb ? Routes.stack1 : Routes.stack,
+    "路由 Navigator 2.0（ShellRoute）: 匹配路由路径切换":
+        GetPlatform.isWeb ? Routes.shell1 : Routes.shell,
     "路由参数传递": Routes.arguments,
     "页面调整动画": Routes.transition,
   };
